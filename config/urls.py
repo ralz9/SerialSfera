3e714ca1ba91e25a2aa039d3a51d4ea66b6b9d06
ralz9 <ralz9-ralz9@mail.ru>
2023-10-10 16:30:51 +0600

    swager

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="SerialSfera.kg",
+      default_version='v1',
+      description="Contacs is we"
+                  "we for WhatsApp: +996 507812318 \n"
+                  "we for instagram SerialSfera.kg \n"
+                  "we for telegram SerialSfera.kg \n "
+                  "we for facebook SerialSfera.kg \n ",
+      contact=openapi.Contact(email="SerialSfera@2000.com"),
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/account/', include('applications.account.urls')),
     path('api/v1/series/', include('applications.series.urls')),
+    path('swagger/', schema_view.with_ui('swagger')),
 
 ]
