a53f28b5fa69f34417c139926eb1e8cc18edc14f
ralz9 <ralz9-ralz9@mail.ru>
2023-10-18 14:56:58 +0600

    category

--- applications/series/models.py
+++ applications/series/models.py
@@ -14,7 +14,7 @@ User = get_user_model()
 
 class Category(models.Model):
 
-    name = models.SlugField(primary_key=True, unique=True, max_length=50)
+    name = models.SlugField(primary_key=True, max_length=50)
     parent = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
     image = models.ImageField(upload_to='category_images/', null=True, blank=True)
 
